a5f804286da112895bc95cb2307bd6de0a057eea
Georg Schuppe <gschuppe@welfenlab.de>
2015-11-23 11:24:08 +0100

    (cleanup) removed old test code

--- test/atomics.js
+++ test/atomics.js
@@ -8,17 +8,4 @@ describe('Go Code Generator', function () {
     var strg = api.gatherAtomic('./atomics/test.go')
     expect(strg).to.deep.equal('HalloWelt\n')
   })
-
-  it('networkGraphFromAST - definition', function () {
-    var ast = [['def', 'Main', 'A']]
-    var g = api.networkGraphFromAST(ast)
-    expect(g.nodes()).to.have.length(3)
-    expect(g.edges()).to.have.length(2)
-  })
-
-  it('networkGraphFromAST - subordinate', function () {
-    var ast = [['def', 'Main', ['subordinate', 'STDIN', ['subordinate', 'STDIN', 'ADD']]]]
-    api.networkGraphFromAST(ast)
-    expect(false).to.be.true
-  })
 })
