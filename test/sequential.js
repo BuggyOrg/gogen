77d509a491ef5ad9f58bb77c62e2e9d9b6b1352d
Georg Schuppe <georg.schuppe@gmail.com>
2016-05-23 09:36:10 +0200

    (chore) imported compoundify

--- test/sequential.js
+++ test/sequential.js
@@ -6,6 +6,7 @@ var api = require('../src/api.js')
 var expect = require('chai').expect
 const exec = require('child_process').exec
 // import _ from 'lodash'
+import { compoundify } from '@buggyorg/graphtools'
 
 var executeCodePromise = (execution, expectation) => {
   return new Promise((resolve, reject) => {
@@ -38,6 +39,7 @@ describe('Sequential Go Code Generator', function () {
 describe('Sequential Go Code Generator', function () {
   it('create sequential code - one compound', function () {
     var graph = graphlib.json.read(JSON.parse(fs.readFileSync('test/fixtures/real_inc.json')))
+    graph = compoundify.compoundify(graph, ['inc:add', 'inc:add_PORT_s1', 'inc:add_PORT_s2', 'inc:add_PORT_sum'])
     return api.preprocess(graph).then((newGraph) => {
       var code = sequential.generateCode(newGraph)
       fs.writeFileSync('test/fixtures/seq-one-compounds.go', code)
