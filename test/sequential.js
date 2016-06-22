6de21101f000ba8c78d557e1d9d38d641622a422
Georg Schuppe <georg.schuppe@gmail.com>
2016-06-22 15:40:06 +0200

    (feature) mux detection and capsulation of paths

--- test/sequential.js
+++ test/sequential.js
@@ -4,7 +4,7 @@ var fs = require('fs')
 var sequential = require('../src/sequential.js')
 var expect = require('chai').expect
 const exec = require('child_process').exec
-// import _ from 'lodash'
+import _ from 'lodash'
 import api from '../src/api.js'
 
 var executeCodePromise = (execution, expectation) => {
@@ -22,7 +22,7 @@ var executeCodePromise = (execution, expectation) => {
   })
 }
 
-describe.only('Sequential Go Code Generator', function () {
+describe('Sequential Go Code Generator', function () {
   it('no compounds', function () {
     var graph = graphlib.json.read(JSON.parse(fs.readFileSync('test/fixtures/real_add.json')))
     return api.preprocess(graph).then((newGraph) => {
@@ -57,17 +57,18 @@ describe.only('Sequential Go Code Generator', function () {
     })
   })
 
-  it('automatic compoundify', function () {
-    var graph = graphlib.json.read(JSON.parse(fs.readFileSync('test/fixtures/real_add.json')))
+  it.only('automatic compoundify', function () {
+    var graph = graphlib.json.read(JSON.parse(fs.readFileSync('test/fixtures/simple-rec.json')))
     graph = sequential.autoCompoundify(graph)
-    fs.writeFileSync('test/fixtures/seq-auto-comp.json', JSON.stringify(graphlib.json.write(graph)))
+    // fs.writeFileSync('test/fixtures/seq-auto-comp.json', JSON.stringify(graphlib.json.write(graph)))
+    /*
     return api.preprocess(graph).then((newGraph) => {
       var code = sequential.generateCode(newGraph)
       fs.writeFileSync('test/fixtures/seq-auto-compound.go', code)
       return executeCodePromise('echo 4 | go run test/fixtures/seq-auto-compound.go', '21').then(() => {
         return executeCodePromise('go fmt test/fixtures/seq-auto-compound.go', 'test/fixtures/seq-auto-compound.go\n')
       })
-    })
+    }) */
   })
 
   /*
